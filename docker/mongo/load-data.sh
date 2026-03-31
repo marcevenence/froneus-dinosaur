204924546c87e794397c1cc175e3cd11c593211a
Marcelo Miño <marcelo.minio@gmail.com>
2026-03-31 12:00:13 -0300

    docker-compose config

---
+++ docker/mongo/load-data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Ejecuta un insertMany en la base dinosaurs
+docker exec -i mongo mongosh -u mongoexpress -p secret123 --authenticationDatabase admin <<EOF
+use dinosaurs_db
+db.dinosaurs.insertMany([
+  {
+      name: "Tyrannosaurus Rex",
+      species: "Theropod",
+      discoveryDate: ISODate("1902-01-01T23:59:59Z"),
+      extinctionDate: ISODate("2023-12-31T23:59:59Z"),
+      status: "ALIVE"
+    },
+    {
+      name: "Triceratops",
+      species: "Ceratopsid",
+      discoveryDate: ISODate("1889-01-01T00:00:00Z"),
+      extinctionDate: ISODate("2023-12-31T23:59:59Z"),
+      status: "ALIVE"
+    },
+    {
+      name: "Velociraptor",
+      species: "Dromaeosaurid",
+      discoveryDate: ISODate("1924-01-01T00:00:00Z"),
+      extinctionDate: ISODate("2023-12-31T23:59:59Z"),
+      status: "ALIVE"
+    }
+])
+EOF
