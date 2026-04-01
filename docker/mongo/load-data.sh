820efc718de94d04f0c1895092297361e076d67c
Marcelo Miño <marcelo.minio@gmail.com>
2026-04-01 14:59:22 -0300

    refactor dockerfiles

--- docker/mongo/load-data.sh
+++ docker/mongo/load-data.sh
@@ -46,4 +46,48 @@ db.dinosaurs.insertMany([
       status: "ALIVE"
     }
 ])
+
+db = db.getSiblingDB('dinosaurs_db_test');
+
+db.createUser({
+  user: 'froneus',
+  pwd: 'secret123',
+  roles: [
+    {
+      role: 'readWrite',
+      db: 'dinosaurs_db_test',
+    },
+  ],
+});
+
+db.createCollection('dinosaurs');
+
+// indexes
+db.dinosaurs.createIndex({ name: 1 }, { unique: true });
+db.dinosaurs.createIndex({ extinctionDate: 1 });
+
+db.dinosaurs.insertMany([
+  {
+      _id: ObjectId('69cc0a3b3ad0e659fad805db'),
+      name: "Tyrannosaurus Rex",
+      species: "Theropod",
+      discoveryDate: ISODate("1902-01-01T23:59:59Z"),
+      extinctionDate: ISODate("2023-12-31T23:59:59Z"),
+      status: "ALIVE"
+    },
+    {
+      name: "Triceratops",
+      species: "Ceratopsid",
+      discoveryDate: ISODate("1889-01-01T00:00:00Z"),
+      extinctionDate: ISODate("2023-12-31T23:59:59Z"),
+      status: "ALIVE"
+    },
+    {
+      name: "Velociraptor",
+      species: "Dromaeosaurid",
+      discoveryDate: ISODate("1924-01-01T00:00:00Z"),
+      extinctionDate: ISODate("2023-12-31T23:59:59Z"),
+      status: "ALIVE"
+    }
+])
 EOF
